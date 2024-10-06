f6309d6bd9b90c34103f2745aad2b6b70013bc3d
Antonio Hohnjec <antonio.hohnjec@fer.hr>
2024-10-06 02:51:46 +0200

    Set basic looks for website frontend

--- website/projects.php
+++ website/projects.php
@@ -2,61 +2,63 @@
 <html>
 <head>
     <title>Projects</title>
-    <link rel="icon" type="image/x-icon" href="website/slike/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="slike/favicon.ico">
     <meta charset = "UTF-8">
     <meta name="author" content="Antonio Hohnjec">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel = "stylesheet" href = "bootstrap/css/bootstrap.min.css">
-    <script src = "bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel = "stylesheet" href = "bootstrap_projects/css/bootstrap.min.css">
+    <script src = "bootstrap_projects/js/bootstrap.min.js"></script>
     <link rel = "stylesheet" type = "text/css" href = "css/app.css">
 </head>
 <body lang="HR">
-    
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img src = "/slike/logo.png" style="height: 50px;"></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-    
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active mx-lg-4 mx-auto my-3">
-                    <a class="nav-link" href="#info">Informacije</a>
-                </li>
-                <li class="nav-item active mx-lg-4 mx-auto my-3">
-                    <a class="nav-link" href="#kontakt">Kontakt</a>
-                </li>
-                <li class="nav-item active mx-lg-4 mx-auto my-3">
-                    <a class="nav-link" href="#">Solutions</a>
-                </li>
-    
-            </ul>
-        </div>
-    </nav>
+    <div class = "cant">
+        <div class = "section">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#"><img src = "slike/logo.png" style="height: 50px;" alt = "logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-   <div class = "project">
-            <div class = "noproject">
-                <h1>Maze</h1>
-                <img src="slike/mazeimg.png" alt="Image of a Maze application">
-                <p><b>Description:</b><br>
-                Aplikacija za generiranje labirinta. Iskušajte vlastito
-                strpljenje, koncentraciju i moć opažanja uz različite razine labirinta. Labirint je nasumično
-                generiran te svakim pokušajem osigurava se novi izazov. Aplikacija nudi i različite načine generiranja
-                labirinta čime možemo na jednostavnijim i složenijim primjerima vidjeti prednosti i nedostatke pojedinog
-                algoritma.</p>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active mx-lg-4 mx-auto my-3">
+                            <a class="nav-link" href="../index.php">Početna stranica</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+            <div style="height: 600px; background-image: url('slike/pocetna.jpg'); background-repeat: no-repeat; background-size: cover; display: flex; justify-content: center;text-shadow: 2px 2px rgb(0,0,0);">
+                <h1 style = "color: white; text-align: center; margin: auto;">U nastavku slijedi popis aplikacija sa linkovima na njihovu stranicu.</h1>
             </div>
         </div>
-        <div class = "project">
-            <a>
-                <h1>HuntersDispatched</h1>
-            </a>
+        <div class = "section rest">
+            <div class = "project">
+                <h1>Maze</h1>
+                <a>
+                    <img src="slike/mazeimg.png" alt="Image of a Maze application">
+                </a>
+                <p><b>Opis:</b><br>
+                    Aplikacija za generiranje labirinta. Iskušajte vlastito
+                    strpljenje, koncentraciju i moć opažanja uz različite razine labirinta. Labirint je nasumično
+                    generiran te svakim pokušajem osigurava se novi izazov. Aplikacija nudi i različite načine generiranja
+                    labirinta čime možemo na jednostavnijim i složenijim primjerima vidjeti prednosti i nedostatke pojedinog
+                    algoritma.</p>
+            </div>
         </div>
-        
-        <footer>
-            Projects made by Antonio Hohnjec &copy; 2020. - 
-        </footer>
+        <div class = "section rest">
+            <div class = "project">
+                <a>
+                    <h1>HuntersDispatched</h1>
+                </a>
+            </div>
+            <footer>
+                Projects made by Antonio Hohnjec &copy; 2020. -
+            </footer>
+        </div>
 
-    <script src = "website/script.js"></script>
+    </div>
 </body>
 
 
